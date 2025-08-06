fab164d27b32d8c60de41ccf868fdf5b9ef0855e
Purcarea Sabina <sabina.purcarea@reevotech.com>
2025-08-06 13:58:21 +0300

    feat: spin button adjustements

--- src/SpinButton.ts
+++ src/SpinButton.ts
@@ -1,39 +1,73 @@
 import { Sprite, Container, Texture } from "pixi.js";
 import playButton from "../assets/PLAY.png";
 import disabledButton from "../assets/PLAY_DISABLED.png";
+import { gsap } from "gsap";
 
+/**
+ * Spin Button class
+ */
 export class SpinButton extends Container {
-  private _playSprite!: Sprite;
-  private _playDisabledSprite!: Sprite;
+  private _buttonSprite!: Sprite;
   public static pressedButtonHandler: Function;
   constructor() {
     super();
-    this.addButton();
+    this.initButton();
     this.setupEvents();
   }
 
   /**
    * Creates the play button
    */
-  private addButton(): void {
-    this._playSprite = new Sprite(Texture.from(playButton));
-    this.addChild(this._playSprite);
-    this._playSprite.eventMode = "static";
-    this._playSprite.cursor = "pointer";
-    this.x = 500;
-    this.y = 200;
-  }
-  /**
-   * Attaches pointerdown event to trigger onPress
-   */
-  private setupEvents(): void {
-    this._playSprite.on("pointerdown", () => this.onPress());
+  private initButton(): void {
+    this._buttonSprite = new Sprite(Texture.from(playButton));
+    this._buttonSprite.eventMode = "static";
+    this._buttonSprite.cursor = "pointer";
+    this.position.set(500, 200);
+    this._buttonSprite.anchor.set(0.5);
+
+    this.addChild(this._buttonSprite);
   }
 
   /**
-   * Handler invoked when the button is pressed
+   * Registers pointer event for button press
+   */
+  private setupEvents(): void {
+    this._buttonSprite.on("pointerdown", () => this.onPress());
+  }
+
+  /**
+   * Plays button press animation and triggers handler
    */
   public onPress(): void {
+    gsap.to(this._buttonSprite.scale, {
+      x: 0.9,
+      y: 0.9,
+      duration: 0.1,
+      ease: "power1.out",
+      yoyo: true,
+      repeat: 1,
+    });
+
     SpinButton.pressedButtonHandler();
+  }
+
+  /**
+   * Disables the button
+   */
+  public disable(): void {
+    this._buttonSprite.texture = Texture.from(disabledButton);
+    this.interactive = false;
+    this._buttonSprite.eventMode = "none";
+    this._buttonSprite.cursor = "default";
+  }
+
+  /**
+   * Enables the button
+   */
+  public enable(): void {
+    this._buttonSprite.texture = Texture.from(playButton);
+    this.interactive = true;
+    this._buttonSprite.eventMode = "static";
+    this._buttonSprite.cursor = "pointer";
   }
 }
